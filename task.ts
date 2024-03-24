ca85c00546c59fd235982a41816d65950ee3e607
Hayma-tul-yusra <164422986+Hayma-tul-yusra@users.noreply.github.com>
2024-03-25 02:17:25 +0500

    Add files via upload

---
+++ task.ts
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const ans:{numberone:number,
+    oprator: string} = await inquirer.prompt([
+
+    {type:"list",
+    name: "oprator",
+    message: "Enter which currency you want to change",
+    choices:["USD Dollar","RIYAL","AED","Omani Rial"]},
+    {
+    type: "number",
+    name: "numberone",
+    message: "Enter your Amount",
+}
+    ])
+    const{ oprator, numberone}= ans;
+    let result 
+    switch (oprator) {
+        case "USD Dollar":
+            result = numberone * 278.25;
+            console.log("Thank you for using our program");
+            break;
+    
+        case "RIYAL":
+            
+            result = numberone * 74.19;
+            console.log("Thank you for using our program");
+            break;
+        case "AED":
+            result = numberone * 76.22;
+            console.log("Thank you for using our program");
+            break;
+            case "Omani Rial":
+                result = numberone * 722.79;
+                console.log("Thank you for using our program");
+                break;
+            
+        default:
+            console.log("choose correct oprator");
+                      
+    }
+    console.log(`${numberone}, ${oprator}, ${ result} into PKR `);
